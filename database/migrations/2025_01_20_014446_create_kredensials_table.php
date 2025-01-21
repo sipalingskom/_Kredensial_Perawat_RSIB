e7d902a81d149946b1ea58a3be099039740b1f25
sipalingskom <saptarino708@gmail.com>
2025-01-22 03:54:40 +0800

    update

--- database/migrations/2025_01_20_014446_create_kredensials_table.php
+++ database/migrations/2025_01_20_014446_create_kredensials_table.php
@@ -13,7 +13,16 @@ return new class extends Migration
     {
         Schema::create('kredensials', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('isi_sub_header_perawatan_id');
+            $table->enum('diminta', ['0','1']);
+            $table->enum('rekomendasi_m', ['0','1']);
+            $table->enum('rekomendasi_ds', ['0','1']);
+            $table->date('tanggal');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('isi_sub_header_perawatan_id')->references('id')->on('isi_sub_header_perawatans')->onDelete('cascade');
         });
     }
 
